59a0eb3111668f620a6eecd78c6c29c6091c41f3
Kowaine <31977607+Kowaine@users.noreply.github.com>
2019-05-31 10:46:14 +0800

    优化主活动并添加注释

--- app/src/main/java/com/example/helloworld/HelloWorldActivity.java
+++ app/src/main/java/com/example/helloworld/HelloWorldActivity.java
@@ -1,10 +1,13 @@
 package com.example.helloworld;
 
+import android.content.ClipData;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -23,12 +26,31 @@ public class HelloWorldActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // 装载菜单项
         getMenuInflater().inflate(R.menu.main, menu);
+
+        MenuView.ItemView item = findViewById(R.id.version);
+
+        // 获取版本号
+        PackageManager packageManager = getPackageManager();
+        String packageName = getPackageName();
+        String version = null;
+        try {// try catch 语句是必需的
+            version = packageManager.getPackageInfo(packageName, PackageManager.GET_CONFIGURATIONS).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        item.setTitle("版本号：" + version);
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        /*响应菜单的点击*/
+
+        // 判断选择项并响应
         switch(item.getItemId())
         {
             case R.id.github_item:
@@ -47,15 +69,22 @@ public class HelloWorldActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        Log.d("run", "enter onActivityResult");
+        /*响应返回Intent*/
+
+        // 判断返回来源
         switch(requestCode)
         {
             case 0:
+                // 判断返回结果
                 if(resultCode == RESULT_OK)
                 {
                     Toast.makeText(HelloWorldActivity.this,data.getIntExtra("num1", 0)+"+"
                             +data.getIntExtra("num2", 0)+"="
                             +data.getIntExtra("result", 0), Toast.LENGTH_SHORT ).show();
+                }
+                else if(resultCode == RESULT_CANCELED)
+                {
+                    Toast.makeText(HelloWorldActivity.this, R.string.cal_canceled, Toast.LENGTH_SHORT);
                 }
                 break;
             default:
@@ -63,15 +92,19 @@ public class HelloWorldActivity extends AppCompatActivity {
         }
     }
 
-    //按动按键更换字符串
+
     public void changeString(View v)
     {
-        TextView text = (TextView) findViewById(R.id.hello_world_text);
+        /*按动按键更换字符串*/
+
+        TextView text = findViewById(R.id.hello_world_text);
         Toast info;
+
+        // 判断文字并切换到另一个
         if(text.getText() == getResources().getString(R.string.hello_string))
         {
-            text.setText(R.string.changed_string);
-            info = Toast.makeText(HelloWorldActivity.this, R.string.changed_string, Toast.LENGTH_SHORT);
+            text.setText(R.string.goodbye_string);
+            info = Toast.makeText(HelloWorldActivity.this, R.string.goodbye_string, Toast.LENGTH_SHORT);
         }
         else
         {
@@ -81,22 +114,39 @@ public class HelloWorldActivity extends AppCompatActivity {
         info.show();
     }
 
-    public void openBrowser(View v)
+    public void openGithubHome(View v)
     {
+        // 隐式Intent打开浏览器访问github主页
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse(getResources().getString(R.string.github_home)));
         startActivity(intent);
     }
 
-    public void changeActivity(View v)
+    public void startCalculateActivity(View v)
     {
-        Log.d("run", "enter changeActivity");
-        int num1 = ((EditText)findViewById(R.id.num1)).getText().toString().equals("")?0:Integer.parseInt(((EditText)findViewById(R.id.num1)).getText().toString());
-        int num2 = ((EditText)findViewById(R.id.num2)).getText().toString().equals("")?0:Integer.parseInt(((EditText)findViewById(R.id.num2)).getText().toString());;
-        Intent intent = new Intent("com.example.hellowold.CALCULATE");
-        intent.putExtra("num1", num1);
-        intent.putExtra("num2", num2);
-        startActivityForResult(intent, 0);
-        Log.d("run", "quit changeActivity");
+        /*将两个数字隐式Intent传送*/
+
+        // 获取输入栏内两个数字，为空则提示用户输入
+        String num_string1 = ((EditText)findViewById(R.id.num1)).getText().toString();
+        String num_string2 = ((EditText)findViewById(R.id.num2)).getText().toString();
+        if(num_string1.equals("") || num_string2.equals(""))
+        {
+            // 提示用户输入
+            Toast.makeText(HelloWorldActivity.this, R.string.null_input, Toast.LENGTH_SHORT);
+        }
+        else
+        {
+            // 正常传输数据
+
+            int num1 = Integer.parseInt(num_string1);
+            int num2 = Integer.parseInt(num_string2);
+
+            // 设置Intent参数
+            Intent intent = new Intent("com.example.hellowold.CALCULATE");
+            intent.putExtra("num1", num1);
+            intent.putExtra("num2", num2);
+
+            startActivityForResult(intent, 0);
+        }
     }
 }
